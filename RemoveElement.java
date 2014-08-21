4c38506b3d0c9bcc0c778962cdfb42d55b456c74
kaiyiliu <kyliu.nju@gmail.com>
2014-08-21 11:19:50 -0400

    Create RemoveElement.java

---
+++ RemoveElement.java
@@ -0,0 +1,13 @@
+public class RemoveElement {
+    public int removeElement(int[] A, int elem) {
+        if (A == null || A.length == 0)
+            return 0;
+        int i = 0;
+        for (int j = 0; j < A.length; j++) {
+            if (A[j] != elem) {
+                A[i++] = A[j];
+            }
+        }
+        return i;
+    }
+}
