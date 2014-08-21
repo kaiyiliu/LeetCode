bb9ba176bd8b485a85db15584fc5437e82b3f737
kaiyiliu <kyliu.nju@gmail.com>
2014-08-21 11:33:09 -0400

    Create ReverseInteger.java

---
+++ ReverseInteger.java
@@ -0,0 +1,20 @@
+public class ReverseInteger {
+    public int reverse(int x) {
+    // notice the overflow issue
+    
+        if (x == 0)
+            return x;
+        int rev = 0;
+        boolean isNeg = false;
+        if (x < 0)
+            isNeg = true;
+        x = Math.abs(x);
+        rev += x % 10;
+        x /= 10;
+        while (x > 0) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        return (isNeg) ? -rev : rev;
+    }
+}
