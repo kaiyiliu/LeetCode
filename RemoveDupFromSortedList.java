e7817bee333f9d4e2ad532457aef9e32611485d7
kaiyiliu <kyliu.nju@gmail.com>
2014-08-22 13:53:53 -0400

    Create RemoveDupFromSortedList.java

---
+++ RemoveDupFromSortedList.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class RemoveDupFromSortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null)
+            return null;
+        ListNode itr = head;
+        while (itr != null && itr.next != null) {
+            ListNode ahead = itr;
+            while (ahead.next != null && ahead.next.val == ahead.val)
+                ahead = ahead.next;
+            itr.next = ahead.next;
+            itr = itr.next;
+        }
+        return head;
+    }
+}
