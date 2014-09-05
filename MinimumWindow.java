9f923703a12cf0ba0223b5bcffa092f5d33ace2d
kaiyiliu <kyliu.nju@gmail.com>
2014-09-04 20:33:19 -0400

    Update MinimumWindow.java

--- MinimumWindow.java
+++ MinimumWindow.java
@@ -1,7 +1,7 @@
 public class MinimumWindow {
     public String minWindow(String S, String T) {
         if (S == null || T == null || T.length() > S.length())
-            return null;
+            return "";
         if (T.length() == 0)
             return "";
         HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
@@ -12,46 +12,47 @@ public class MinimumWindow {
                 hm.put(T.charAt(i), -1);
         }
         int[] p = new int[2];
-        int dist = Integer.MAX_VALUE;
         int front = 0;
         int end = 0;
         int count = 0;
-        while (front < S.length() && count != T.length()) {
+        // front will be pointed at the front of the window
+        while (front < S.length()) {
             if (hm.containsKey(S.charAt(front))) {
-                hm.put(S.charAt(front), hm.get(S.charAt(front) + 1));
-                count++;
-                if (count == 1)
-                    end = front;
+                hm.put(S.charAt(front), hm.get(S.charAt(front)) + 1);
+                if (hm.get(S.charAt(front)) <= 0) {
+                    count++;
+                    if (count == 1)
+                        end = front;
+                    if (count == T.length())
+                        break;
+                }
             }
             front++;
         }
-        System.out.print(end);
-        if (count < T.length())
+        if (count != T.length())
             return "";
         p[0] = end;
-        p[1] = --front;
-        dist = front - end;
-        while (end <= front && front < S.length()) {
-            if (hm.get(S.charAt(end)) > 0) {
+        p[1] = front;
+        int dist = front - end;
+        while (true) {
+            while (hm.get(S.charAt(end)) > 0) {
                 hm.put(S.charAt(end), hm.get(S.charAt(end)) - 1);
                 end++;
                 while (end <= front && !hm.containsKey(S.charAt(end)))
                     end++;
-                if (end > front)
-                    break;
                 if (front - end < dist) {
                     dist = front - end;
                     p[0] = end;
                     p[1] = front;
                 }
-            } else {
-                front++;
-                while (front < S.length() && !hm.containsKey(S.charAt(front)))
-                    front++;
-                if (front >= S.length())
-                    break;
-                hm.put(S.charAt(front), hm.get(S.charAt(front)) + 1);
             }
+            front++;
+            while (front < S.length() && !hm.containsKey(S.charAt(front)))
+                front++;
+            if (front >= S.length())
+                break;
+            hm.put(S.charAt(front), hm.get(S.charAt(front)) + 1);
+             
         }
         return S.substring(p[0], p[1] + 1);
     }
