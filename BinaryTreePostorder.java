f526066359ecb17f325b7cae5ae3360886593e1e
kaiyiliu <kyliu.nju@gmail.com>
2014-09-13 16:50:31 -0400

    Update BinaryTreePostorder.java

--- BinaryTreePostorder.java
+++ BinaryTreePostorder.java
@@ -7,18 +7,20 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-public class BinaryTreePostorder {
+public class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> result = new LinkedList<Integer>();
-        traverse(root, result);
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        s.push(root);
+        while (!s.isEmpty()) {
+            TreeNode node = s.pop();
+            if (node == null)
+                continue;
+            result.add(node.val);
+            s.push(node.left);
+            s.push(node.right);
+        }
+        Collections.reverse(result);
         return result;
-    }
-    
-    private void traverse(TreeNode node, List<Integer> result) {
-        if (node == null)
-            return;
-        traverse(node.left, result);
-        traverse(node.right, result);
-        result.add(node.val);
     }
 }
