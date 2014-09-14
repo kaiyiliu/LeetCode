41f4eabb759d63cb77a6d47b1301ab9805498f2a
kaiyiliu <kyliu.nju@gmail.com>
2014-09-13 20:05:42 -0400

    Create LinkedListCycle2.java

---
+++ LinkedListCycle2.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class LinkedListCycle2 {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null)
+            return null;
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                while (head != slow) {
+                    head = head.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
